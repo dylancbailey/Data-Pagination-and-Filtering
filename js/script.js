03877781e70426070172b422e10238a96c0690b6
Dylan Bailey <wearesleepless@gmail.com>
2022-10-27 16:39:58 -0400

    Adds showPage function

--- js/script.js
+++ js/script.js
@@ -11,13 +11,41 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-
+// for (let [index, value] of data.entries()) {
+//    console.log(value.name, index);
+// }
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page) {
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
+   const student = document.querySelector(".student-list");
 
+   student.innerHTML = "";
+
+   for (let [index, value] of list.entries()) {
+
+      if (index >= startIndex && index < endIndex) {
+         student.insertAdjacentHTML("beforeend", 
+         `
+         <li class="student-item cf">
+            <div class="student-details">
+            <img class="avatar" src="${value.picture.large}" alt="Profile Picture">
+            <h3>${value.name.first} ${value.name.last}</h3>
+            <span class="email">${value.email}</span>
+            </div>
+            <div class="joined-details">
+            <span class="date">Joined ${value.registered.date}</span>
+            </div>
+         </li>
+         `
+         );
+      }
+   }
+}
 
 
 /*
@@ -28,3 +56,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showPage(data, 1);
